425c0c81d2063ce50784ea78645283a793edf349
adalberto.silva <adalberto.silva@concrete.com.br>
2017-03-30 18:03:13 -0300

    set up start script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -20,10 +21,6 @@ app.use((req, res, next) => {
 
   next();
 });
-
-// app.use((req, res, next) => {
-//   res.render('maintaince.hbs')
-// });
 
 app.use(express.static(__dirname + '/public'));
 
@@ -56,6 +53,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });

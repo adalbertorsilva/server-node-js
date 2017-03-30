ede9e4e61df1b0ab51c2928e8ac631727d825dbc
adalberto.silva <adalberto.silva@concrete.com.br>
2017-03-30 18:13:34 -0300

    creation of projects page

--- server.js
+++ server.js
@@ -53,6 +53,13 @@ app.get('/bad', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs',{
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Portfolio page here!'
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
